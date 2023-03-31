4d59aa2e099ec6c12f55383f92bc10c5ec600d2b
shortarrow <bamboogeneral@shortarrow.jp>
2023-04-01 04:48:16 +0900

    add rainbow

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,137 @@ import * as vscode from "vscode";
 const vsCodeGlobal = vscode.ConfigurationTarget
   .Global as vscode.ConfigurationTarget;
 
+/**
+ * get is dark theme
+ * @returns if dark theme, return true
+ */
 function getIsDark() {
   return vscode.window.activeColorTheme.kind === vscode.ColorThemeKind.Dark;
 }
 
+/**
+ * shift hue of color code
+ * @param colorCode color code (e.g. #ffffff)
+ * @param shift shift value (0～11)
+ * @returns shifted color code (e.g. #ffffff)
+ */
+function shiftHue(colorCode: string, shift: number) {
+  const [r, g, b] = rgbFromColorCode(colorCode);
+  const [h, s, l] = hslFromRgb(r, g, b);
+  const huePerStep = 360 / 16;
+  const hue = (((h + ( shift) * huePerStep) % 360) + 360) % 360;
+  return colorCodeFromHsl(hue, s, l);
+}
+
+/**
+ * get rgb value from color code
+ * @param colorCode color code (e.g. #ffffff)
+ * @returns RGB value array [r, g, b]
+ */
+function rgbFromColorCode(colorCode: string): number[] {
+  const hex = colorCode.slice(1);
+  const r = parseInt(hex.slice(0, 2), 16);
+  const g = parseInt(hex.slice(2, 4), 16);
+  const b = parseInt(hex.slice(4, 6), 16);
+  return [r, g, b];
+}
+
+/**
+ * RGB value to HSL value conversion helper function
+ * @param r red component (0～255)
+ * @param g green component (0～255)
+ * @param b blue component (0～255)
+ * @returns HSL value array [h, s, l]
+ */
+function hslFromRgb(r: number, g: number, b: number): number[] {
+  const min = Math.min(r, g, b) / 255;
+  const max = Math.max(r, g, b) / 255;
+  const delta = max - min;
+
+  let h, s, l;
+
+  if (delta === 0) {
+    h = 0;
+  } else if (max === r / 255) {
+    h = 60 * ((g / 255 - b / 255) / delta);
+  } else if (max === g / 255) {
+    h = 60 * ((b / 255 - r / 255) / delta + 2);
+  } else {
+    h = 60 * ((r / 255 - g / 255) / delta + 4);
+  }
+
+  if (h < 0) {
+    h += 360;
+  }
+
+  l = (max + min) / 2;
+
+  if (delta === 0) {
+    s = 0;
+  } else {
+    s = delta / (1 - Math.abs(2 * l - 1));
+  }
+
+  return [h, s, l];
+}
+
+/**
+ * Hsl to #?????? formatted string translation helper function
+ * @param h hue
+ * @param s saturation
+ * @param l luminescence
+ * @returns #?????? formatted string
+ */
+function colorCodeFromHsl(h: number, s: number, l: number): string {
+  const [r, g, b] = rgbFromHsl(h, s, l);
+  return rgbToColorCode(r, g, b);
+}
+
+/**
+ * Hsl to rgb translation helper function 
+ * @param h 
+ * @param s 
+ * @param l 
+ * @returns HSL value array [h, s, l]
+ */
+function rgbFromHsl(h: number, s: number, l: number): [number, number, number] {
+  const c = (1 - Math.abs(2 * l - 1)) * s;
+  const hp = h / 60;
+  const x = c * (1 - Math.abs((hp % 2) - 1));
+  const [r1, g1, b1] =
+    hp < 1
+      ? [c, x, 0]
+      : hp < 2
+      ? [x, c, 0]
+      : hp < 3
+      ? [0, c, x]
+      : hp < 4
+      ? [0, x, c]
+      : hp < 5
+      ? [x, 0, c]
+      : [c, 0, x];
+  const m = l - 0.5 * c;
+  const [r, g, b] = [r1 + m, g1 + m, b1 + m];
+  return [Math.round(255 * r), Math.round(255 * g), Math.round(255 * b)];
+}
+
+/**
+ * rgb to #?????? formatted string translation helper function
+ * @param r 
+ * @param g 
+ * @param b 
+ * @returns 
+ */
+function rgbToColorCode(r: number, g: number, b: number): string {
+  return "#" + [r, g, b].map((x) => x.toString(16).padStart(2, "0")).join("");
+}
+
+/**
+ * update relative line numbers
+ * @param editor 
+ * @param decorationType 
+ * @returns 
+ */
 async function updateRelativeLineNumbers(
   editor: vscode.TextEditor | undefined,
   decorationType: vscode.TextEditorDecorationType
@@ -21,6 +148,8 @@ async function updateRelativeLineNumbers(
   const document = editor.document;
   const activeLineNumberColor = getActiveLineNumberColor();
   const inactiveLineNumberColor = getInactiveLineNumberColor();
+  const enableRainbow = getEnableRainbow();
+  const centerColorOfRainbow = getCenterColorOfRainbow();
 
   const labelWidth = document.lineCount.toString().length;
 
@@ -42,6 +171,11 @@ async function updateRelativeLineNumbers(
           contentText: label,
           color: isCurrentLine
             ? activeLineNumberColor
+            : enableRainbow
+            ? shiftHue(
+                centerColorOfRainbow,
+                Math.abs(lineIndex - activeLineNumber)
+              )
             : inactiveLineNumberColor,
           textDecoration: `
             box-sizing: border-box;
@@ -60,9 +194,19 @@ async function updateRelativeLineNumbers(
   editor.setDecorations(decorationType, enableRlativeLine ? decorations : []);
 }
 
+function getEnableRainbow() {
+  const config = vscode.workspace.getConfiguration("LineNumberDeco");
+  return config.get<boolean>("enableRainbow", false);
+}
+
+function getCenterColorOfRainbow() {
+  const config = vscode.workspace.getConfiguration("LineNumberDeco");
+  return config.get<string>("centerColorOfRainbow", "#8888FF");
+}
+
 function getEnableRelativeLineDefault() {
   const config = vscode.workspace.getConfiguration("LineNumberDeco");
-  return config.get<boolean>("enableRlativeLineOnDefault",true);
+  return config.get<boolean>("enableRlativeLineOnDefault", true);
 }
 
 function updateEnableRelativeLineDefault(set: boolean) {
@@ -91,6 +235,9 @@ function getActiveLineNumberColor() {
     : new vscode.ThemeColor("LineNumberDeco.activeForeground");
 }
 
+/**
+ * enable relative line numbers 
+ */
 export function enableDeco() {
   const decorationType = vscode.window.createTextEditorDecorationType({});
   vscode.window.onDidChangeActiveTextEditor((e) => {
@@ -101,6 +248,10 @@ export function enableDeco() {
   });
 }
 
+/**
+ * activate extension
+ * @param context 
+ */
 export function activate(context: vscode.ExtensionContext) {
   enableDeco();
   context.subscriptions.push(
@@ -123,4 +274,7 @@ export function activate(context: vscode.ExtensionContext) {
   );
 }
 
+/**
+ * deactivate extension
+ */
 export function deactivate() {}
