9c8d5d607f197b364e377f963c32dd0e5b3747ad
ShortArrow <bamboogeneral@shortarrow.jp>
2023-08-20 01:27:47 +0900

    add parameters for colors test

--- src/test/colors.test.ts
+++ src/test/colors.test.ts
@@ -1,13 +1,24 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
 import { colorCodeFromHsl } from '../colors';
 
-suite('Colors Test', () => {
-	test('Sample hue', () => {
-		assert.strictEqual(colorCodeFromHsl(0, 1, 0.5), '#ff0000');
-		assert.strictEqual(colorCodeFromHsl(60, 1, 0.5), '#ffff00');
-	});
+suite('Test make color code from hsl value', () => {
+    const testCases = [
+        { input: { h: 0, s: 1, l: 0.5 }, expected: "#ff0000" },
+        { input: { h: 60, s: 1, l: 0.5 }, expected: "#ffff00" },
+        { input: { h: 120, s: 1, l: 0.5 }, expected: "#00ff00" },
+        { input: { h: 180, s: 1, l: 0.5 }, expected: "#00ffff" },
+        { input: { h: 240, s: 1, l: 0.5 }, expected: "#0000ff" },
+        { input: { h: 300, s: 1, l: 0.5 }, expected: "#ff00ff" },
+        { input: { h: 360, s: 1, l: 0.5 }, expected: "#ff0000" },
+        { input: { h: 0, s: 0, l: 1 }, expected: "#ffffff" },
+        { input: { h: 0, s: 0, l: 0 }, expected: "#000000" },
+        { input: { h: 0, s: 0, l: 0.5 }, expected: "#808080" },
+		{ input: { h: 1, s: 1, l: 1 }, expected: "#ffffff" },
+    ];
+    testCases.forEach(({ input, expected }) => {
+		test(`Must become ${JSON.stringify(input)} to ${expected}`, () => {
+			assert.strictEqual(colorCodeFromHsl(input.h,input.s,input.l), expected);
+		});
+    });	
 });
+   
