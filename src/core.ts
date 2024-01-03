fc5558da0a06e231628e81c7a9a8126f412c026e
ShortArrow <bamboogeneral@shortarrow.jp>
2024-01-04 00:15:54 +0900

    Update relative line numbers in core.ts

--- src/core.ts
+++ src/core.ts
@@ -43,43 +43,48 @@ export async function updateRelativeLineNumbers(
   const repeatingDigitsColor = getColorAtRepeatingDigits();
   const centerColorOfRainbow = getColorAtCenterOfRainbow();
   const labelWidth = document.lineCount.toString().length;
+  const endLine = document.lineCount < editor.visibleRanges[0].end.line + 2 ? document.lineCount : editor.visibleRanges[0].end.line + 2;
+  const startLine = editor.visibleRanges[0].start.line - 1 < 0 ? 0 : editor.visibleRanges[0].start.line;
+  for (let lineIndex = startLine; lineIndex < endLine; lineIndex++) {
+    try {
+      const lineRange = document.lineAt(lineIndex).range;
+      const isCurrentLine = lineIndex === activeLineNumber;
 
-  for (let lineIndex = editor.visibleRanges[0].start.line ; lineIndex < editor.visibleRanges[0].end.line + 2; lineIndex++) {
-    const lineRange = document.lineAt(lineIndex).range;
-    const isCurrentLine = lineIndex === activeLineNumber;
+      const label = isCurrentLine
+        ? String(activeLineNumber + 1)
+        : String(Math.abs(lineIndex - activeLineNumber));
 
-    const label = isCurrentLine
-      ? String(activeLineNumber + 1)
-      : String(Math.abs(lineIndex - activeLineNumber));
-
-    const rangeScope = new vscode.Range(lineRange.start, lineRange.start);
-    const lineNumberStyle = {
-      width: `${labelWidth / 2 + 0.5}em`,
-      align: "right",
-      contentText: label,
-      color: isCurrentLine
-        ? activeLineNumberColor
-        : (enableRepeatingDigits && isRepeatingDigits(label))
-          ? repeatingDigitsColor
-          : enableRainbow
-            ? shiftHue(centerColorOfRainbow, Math.abs(lineIndex - activeLineNumber))
-            : inactiveLineNumberColor,
-      textDecoration: `
+      const rangeScope = new vscode.Range(lineRange.start, lineRange.start);
+      const lineNumberStyle = {
+        width: `${labelWidth / 2 + 0.5}em`,
+        align: "right",
+        contentText: label,
+        color: isCurrentLine
+          ? activeLineNumberColor
+          : (enableRepeatingDigits && isRepeatingDigits(label))
+            ? repeatingDigitsColor
+            : enableRainbow
+              ? shiftHue(centerColorOfRainbow, Math.abs(lineIndex - activeLineNumber))
+              : inactiveLineNumberColor,
+        textDecoration: `
             box-sizing: border-box;
             text-align: right;
             padding-right: 1em;
           `,
-      fontWeight: "bold",
-    } as vscode.DecorationInstanceRenderOptions;
-    const lineNumberAreaStyle: vscode.DecorationInstanceRenderOptions = {
-      before: lineNumberStyle,
-    } as vscode.DecorationInstanceRenderOptions;
-    const decoration: vscode.DecorationOptions = {
-      range: rangeScope,
-      renderOptions: lineNumberAreaStyle,
-    };
-
-    decorations.push(decoration);
+        fontWeight: "bold",
+      } as vscode.DecorationInstanceRenderOptions;
+      const lineNumberAreaStyle: vscode.DecorationInstanceRenderOptions = {
+        before: lineNumberStyle,
+      } as vscode.DecorationInstanceRenderOptions;
+      const decoration: vscode.DecorationOptions = {
+        range: rangeScope,
+        renderOptions: lineNumberAreaStyle,
+      };
+      decorations.push(decoration);
+    }
+    catch (error) {
+      console.error(error);
+    }
   }
   const enableRelativeLine = getEnableRelativeLine();
 
